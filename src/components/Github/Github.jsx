5ba52ef0708a8bc8f6c098e09deb643fe920caf2
nafisreza <bigganinafis@gmail.com>
2023-11-08 13:07:34 +0600

    🔥 Optimization with useLoaderData hook

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
+    const data = useLoaderData()
+
+/*
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -11,6 +15,8 @@ function Github() {
         console.log(data);
       });
   }, []);
+*/
+
   return (
     <div className="flex justify-center bg-gray-700 p-5">
       <img className="rounded-full" width="200px" src={data.avatar_url} alt="" />
@@ -22,3 +28,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+    const response = await fetch ('https://api.github.com/users/nafisreza')
+    return response.json()
+}
